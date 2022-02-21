8194352861fd737ef30618a248ef0f03fe9d4171
RobertitoGC <titorobertalto@gmail.com>
2022-02-21 22:07:13 +0100

    primera parte del programa de entrenamiento

---
+++ src/face_recognizer/src/Train_face_recognition_model.py
@@ -0,0 +1,15 @@
+# import the necessary packages
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+import argparse
+import pickle
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--embeddings", required=True,
+	help="path to serialized db of facial embeddings")
+ap.add_argument("-r", "--recognizer", required=True,
+	help="path to output model trained to recognize faces")
+ap.add_argument("-l", "--le", required=True,
+	help="path to output label encoder")
+args = vars(ap.parse_args())
