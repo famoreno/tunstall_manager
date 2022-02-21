fcd40fcdfc40ad371147e9130c3937a8e4948e4d
RobertitoGC <titorobertalto@gmail.com>
2022-02-21 21:53:03 +0100

    he añadido algunos comentarios al código pero ningun cambio sustancial

--- src/face_recognizer/src/detect_faces_video.py
+++ src/face_recognizer/src/detect_faces_video.py
@@ -1,23 +1,53 @@
 from imutils.video import VideoStream
 import numpy as np
 import argparse
-import imutils
-import time
+import time # En el tutorial no aparece esta librería
 import cv2
- 
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
+
+#ap.add_argument("-i", "--dataset", required=True,
+#help="path to inputdirectory of faces + images") # ruta a la entrada del set de datos de las imágenes de caras
+
+#ap.add_argument("-e", "--embeddings", required=True,
+#	help="path to output serialized db of facial embeddings") #ruta de la salida de ambeddings. El código computa estos embeddings faciales y se serializaran al disco
+
+#ap.add_argument("-d", "--detector", required=True,
+	#help="path to OpenCV's deep learning face detector")
+
+#ap.add_argument("-m", "--embedding-model", required=True,
+#	help="path to OpenCV's deep learning face embedding model")
+
+# Aquí los argumentos varían un poco entre el tutorial y el código de git, de momento los he puesto todos y ya vamos viendo si alguno es redundante
+
 ap.add_argument("-p", "--prototxt", required=True,
-    help="path to Caffe 'deploy' prototxt file")
+    help="path to Caffe 'deploy' prototxt file") # Averiguar función del argumento
+
 ap.add_argument("-m", "--model", required=True,
-    help="path to Caffe pre-trained model")
+    help="path to Caffe pre-trained model") # Averiguar función del argumento
+
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
-    help="minimum probability to filter weak detections")
+    help="minimum probability to filter weak detections") # Umbral de filtrado 
 args = vars(ap.parse_args())
 
 # load our serialized model from disk
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+#print("[INFO] loading face detector...")
+#protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"]) # se usa un detector Caffe basado en DL para detectar las caras
+#modelPath = os.path.sep.join([args["detector"],
+#	"res10_300x300_ssd_iter_140000.caffemodel"])
+#detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+# este fragmento de código parece que es redundante, hacer averiguaciones de las diferencias 
+
+# load our serialized face embedding model from disk
+#print("[INFO] loading face recognizer...")
+#embedder = cv2.dnn.readNetFromTorch(args["embedding_model"]) # modelo basado en Torch y responsable de de extraer los embedidos faciales mediante aprendizaje
+
+#fragmento de código del tutorial, posiblemente haya que descomentarlo, pero primero tengo que examinar todo el código
  
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
