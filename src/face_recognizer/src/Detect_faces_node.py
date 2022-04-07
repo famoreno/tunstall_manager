d0a95eae5f5aec405739b145c7928803940c66c6
RobertitoGC <titorobertalto@gmail.com>
2022-04-07 12:43:37 +0200

    ahora si que si el bueno

--- src/face_recognizer/src/Detect_faces_node.py
+++ src/face_recognizer/src/Detect_faces_node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env.python3
+#!/usr/bin/env python3
 # import the necessary packages
 from logging import shutdown
 
@@ -9,7 +9,7 @@ import numpy as np
 import argparse
 import time 
 import cv2
-from std_msgs.msg import String, int
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from .helpers import FACIAL_LANDMARKS_IDXS
@@ -24,12 +24,13 @@ class face_detector_node:
                 self.pub_cara = rospy.Publisher('cara', Image,queue_size=10)
                 self.rate = rospy.Rate(1)
                 self.bridge = CvBridge()
+                self.confidence = 0.9
 
                 while not rospy.is_shutdown():
                     #if self.new_image_ready: Es posible que no cea necesario este comando
                     # grab the frame from the threaded video stream and resize it
                     # to have a maximum width of 400 pixels
-                    frame = vs.read()
+                    frame = self.new_image
                     frame = imutils.resize(frame, width=400)
 
                     # grab the frame dimensions and convert it to a blob
@@ -51,7 +52,7 @@ class face_detector_node:
  
                         # filter out weak detections by ensuring the `confidence` is
                          # greater than the minimum confidence
-                        if confidence < args["confidence"]:
+                        if confidence < self.confidence:
                             continue
                         count += 1
 
@@ -59,13 +60,24 @@ class face_detector_node:
                         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                         (startX, startY, endX, endY) = box.astype("int")
 
+                        
+
                         # draw the bounding box of the face along with the associated probability
-                        text = "{:.2f}%".format(confidence * 100) + ", Count " + str(count)
-                        y = startY - 10 if startY - 10 > 10 else startY + 10
-                        cv2.rectangle(frame, (startX, startY), (endX, endY),(0, 255, 0), 2)
-                        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+                        #text = "{:.2f}%".format(confidence * 100) + ", Count " + str(count)
+                        #y = startY - 10 if startY - 10 > 10 else startY + 10
+                        #cv2.rectangle(frame, (startX, startY), (endX, endY),(0, 255, 0), 2)
+                        #cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
 
-
+                        #recortar la imagen de la cara
+                        crop_frame = frame[startY:endY,startX:endX]
+                        #gdh
+                        # 
+                        # alinear ojos
+                        # 
+                        #   
+                        # Volver a cambiar el formato a imagen
+                        face_frame = self.bridge.cv2_to_imgmsg(crop_frame)
+                        self.pub_cara.publish(face_frame)
 
                     # show the output frame
                     cv2.imshow("Frame", frame)
@@ -76,14 +88,14 @@ class face_detector_node:
                     # break
                     # Estes liíneas para salir al precionar la q imagino que deberían ir fuera
 
-            def callback(self,data):
-		        if self.new_image_ready:
-			        return
-		        try:
-			        self.new_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
-			        self.new_image_ready = True
-		        except CvBridgeError as e:
-			        print(e)
+        def callback(self,data):
+            if self.new_image_ready:
+                return
+            try:
+                self.new_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+                self.new_image_ready = True
+            except CvBridgeError as e:
+                print(e)
 
 def main(args):
     rospy.init_node('face_detector_node', anonymous=True)
