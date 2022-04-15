3d407e210109b8537aa1329a683c991b4388a4ea
Francisco-Angel Moreno <famoreno@uma.es>
2022-04-15 20:04:18 +0200

    Script updated with model use and minor fixes

--- src/face_recognizer/src/Detect_faces_node.py
+++ src/face_recognizer/src/Detect_faces_node.py
@@ -4,30 +4,40 @@ from logging import shutdown
 
 from matplotlib import image
 import rospy 
-from imutils.video import VideoStream
 import numpy as np
 import argparse
-import time 
+import time
+import sys
 import cv2
+
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from .helpers import FACIAL_LANDMARKS_IDXS
-from .helpers import shape_to_np
 
-
-
+import imutils
+from imutils.face_utils.helpers import FACIAL_LANDMARKS_IDXS
+from imutils.face_utils.helpers import shape_to_np
 
 class face_detector_node:
         def __init__(self) -> None:
                 self.sub = rospy.Subscriber('/usb_cam/image_raw',Image,self.callback)
-                self.pub_cara = rospy.Publisher('cara', Image,queue_size=10)
+                self.pub_cara = rospy.Publisher('face', Image,queue_size=10)
                 self.rate = rospy.Rate(1)
                 self.bridge = CvBridge()
                 self.confidence = 0.9
+                self.new_image_ready = False
+
+                # read detector parameters
+                self.protoPath = rospy.get_param('~protoPath')
+                self.modelPath = rospy.get_param('~modelPath')
+                
+                # load face detector
+                self.detector = cv2.dnn.readNetFromCaffe(self.protoPath, self.modelPath)
 
                 while not rospy.is_shutdown():
-                    #if self.new_image_ready: Es posible que no cea necesario este comando
+                    if not self.new_image_ready:
+                        continue
+
                     # grab the frame from the threaded video stream and resize it
                     # to have a maximum width of 400 pixels
                     frame = self.new_image
@@ -37,30 +47,25 @@ class face_detector_node:
                     (h, w) = frame.shape[:2]
                     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,(300, 300), (104.0, 177.0, 123.0))
 
-                    # pass the blob through the network and obtain the detections and
-                     # predictions
-                    net.setInput(blob)
-                    detections = net.forward()
+                    # pass the blob through the network and obtain the detections and predictions
+                    self.detector.setInput(blob)
+                    detections = self.detector.forward()
                     count = 0  
 
                     for i in range(0, detections.shape[2]):
-                        # extract the confidence (i.e., probability) associated with the
-                        # prediction
+                        # extract the confidence (i.e., probability) associated with the prediction
                         confidence = detections[0, 0, i, 2]
-                        #print(confidence * 100)
 
- 
                         # filter out weak detections by ensuring the `confidence` is
-                         # greater than the minimum confidence
+                        # greater than the minimum confidence
                         if confidence < self.confidence:
                             continue
+                        print(confidence * 100)
                         count += 1
 
                         # compute the (x, y)-coordinates of the bounding box for the object
                         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                         (startX, startY, endX, endY) = box.astype("int")
-
-                        
 
                         # draw the bounding box of the face along with the associated probability
                         #text = "{:.2f}%".format(confidence * 100) + ", Count " + str(count)
@@ -68,11 +73,10 @@ class face_detector_node:
                         #cv2.rectangle(frame, (startX, startY), (endX, endY),(0, 255, 0), 2)
                         #cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
 
-                        #recortar la imagen de la cara
+                        # recortar la imagen de la cara
                         crop_frame = frame[startY:endY,startX:endX]
                         
-                        
-                        # ALINEAR OJOS
+                        # TODO: ALINEAR OJOS
                         
                         
                         #   
@@ -80,9 +84,12 @@ class face_detector_node:
                         face_frame = self.bridge.cv2_to_imgmsg(crop_frame)
                         self.pub_cara.publish(face_frame)
 
+                    # clear image ready flag to process more
+                    self.new_image_ready = False
+
                     # show the output frame
-                    cv2.imshow("Frame", frame)
-                    key = cv2.waitKey(1) & 0xFF
+                    # cv2.imshow("Frame", frame)
+                    # key = cv2.waitKey(1) & 0xFF
 
                     # if the `q` key was pressed, break from the loop
                     # if key == ord("q"):
