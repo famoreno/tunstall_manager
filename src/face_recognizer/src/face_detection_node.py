18bd68250e7b29a135218fdf39b0f9f4b19739b8
Francisco-Angel Moreno <famoreno@uma.es>
2022-05-06 11:36:30 +0200

    Face alignment is now working

--- src/face_recognizer/src/face_detection_node.py
+++ src/face_recognizer/src/face_detection_node.py
@@ -5,10 +5,11 @@ from logging import shutdown
 from matplotlib import image
 import rospy 
 import numpy as np
-import argparse
-import time
+# import argparse
+# import time
 import sys
 import cv2
+import dlib
 
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
@@ -17,7 +18,6 @@ from cv_bridge import CvBridge, CvBridgeError
 import imutils
 from imutils.face_utils.helpers import FACIAL_LANDMARKS_IDXS
 from imutils.face_utils.helpers import shape_to_np
-
 from imutils.face_utils import FaceAligner
 
 class face_detector_node:
@@ -29,12 +29,15 @@ class face_detector_node:
                 self.confidence = 0.9
                 self.new_image_ready = False
 
-                # read detector parameters
+                # face detector
                 self.protoPath = rospy.get_param('~protoPath')
                 self.modelPath = rospy.get_param('~modelPath')
-                
-                # load face detector
                 self.detector = cv2.dnn.readNetFromCaffe(self.protoPath, self.modelPath)
+
+                # face landmarks predictor 
+                self.predictorPath = rospy.get_param('~shapePredictor')
+                self.predictor = dlib.shape_predictor(self.predictorPath)
+                self.fa = FaceAligner(self.predictor)
 
                 while not rospy.is_shutdown():
                     if not self.new_image_ready:
@@ -42,8 +45,8 @@ class face_detector_node:
 
                     # grab the frame from the threaded video stream and resize it
                     # to have a maximum width of 400 pixels
-                    frame = self.new_image
-                    frame = imutils.resize(frame, width=400)
+                    frame0 = self.new_image
+                    frame = imutils.resize(frame0, width=800)
 
                     # grab the frame dimensions and convert it to a blob
                     (h, w) = frame.shape[:2]
@@ -60,50 +63,32 @@ class face_detector_node:
 
                         # filter out weak detections by ensuring the `confidence` is
                         # greater than the minimum confidence
+                        print(confidence * 100)
                         if confidence < self.confidence:
                             continue
-                        print(confidence * 100)
                         count += 1
 
                         # compute the (x, y)-coordinates of the bounding box for the object
                         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                         (startX, startY, endX, endY) = box.astype("int")
 
-                        # draw the bounding box of the face along with the associated probability
-                        #text = "{:.2f}%".format(confidence * 100) + ", Count " + str(count)
-                        #y = startY - 10 if startY - 10 > 10 else startY + 10
-                        #cv2.rectangle(frame, (startX, startY), (endX, endY),(0, 255, 0), 2)
-                        #cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
-
                         # recortar la imagen de la cara
-                        crop_frame = frame[startY:endY,startX:endX]
+                        # crop_frame = frame[startY:endY,startX:endX]
 
-                        # TODO poner imagen en escala de grises
-                         #image = imutils.resize(image, width=800)
-                         #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                        image = imutils.resize(frame0, width=800)
+                        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                         
-                        # TODO: ALINEAR OJOS
-                         #fa = FaceAligner(predictor, desiredFaceWidth=256)
-                         #face_frame = fa.align(frame, gray, crop_frame)
-
-                        # volver a recortar 
-                         #crop_frame = face_frame[startY:endY,startX:endX]
+                        # Eye alignment
+                        this_bb = dlib.rectangle(startX,startY,endX,endY)
+                        face_aligned = self.fa.align(image, gray, this_bb)
 
                         # Volver a cambiar el formato a imagen
-                        face_frame = self.bridge.cv2_to_imgmsg(crop_frame)
+                        # face_frame = self.bridge.cv2_to_imgmsg(crop_frame)
+                        face_frame = self.bridge.cv2_to_imgmsg(face_aligned)
                         self.pub_cara.publish(face_frame)
 
                     # clear image ready flag to process more
                     self.new_image_ready = False
-
-                    # show the output frame
-                    # cv2.imshow("Frame", frame)
-                    # key = cv2.waitKey(1) & 0xFF
-
-                    # if the `q` key was pressed, break from the loop
-                    # if key == ord("q"):
-                    # break
-                    # Estes liíneas para salir al precionar la q imagino que deberían ir fuera
 
         def callback(self,data):
             if self.new_image_ready:
