e38796d6763fe683dc8eef5b1868cc1953422da0
RobertitoGC <titorobertalto@gmail.com>
2022-05-12 13:58:02 +0200

    modificadas las rutas de acceso y añadido el nodo de reconocimiento al roslaunch

--- src/face_recognizer/src/Face_recognition_node.py
+++ src/face_recognizer/src/Face_recognition_node.py
@@ -25,61 +25,63 @@ class Face_recognition_node:
 
         # load our serialized face embedding model from disk
         print("[INFO] loading face recognizer...")
-        embbedModelPath = "../../data/openface.nn4.small2.v1.t7"
-        embedder = cv2.dnn.readNetFromTorch(embbedModelPath) # This model is Torch-based and is responsible for extracting facial embeddings via deep learning feature extraction.
+
+        self.embbedModelPath = rospy.get_param('~embbedModelPath')
+        #embbedModelPath = "../../data/openface.nn4.small2.v1.t7"
+        embedder = cv2.dnn.readNetFromTorch(self.embbedModelPath) # This model is Torch-based and is responsible for extracting facial embeddings via deep learning feature extraction.
 
         # load the actual face recognition model along with the label encoder
-        recognizerPath = "../../data/train_output/recognizer.pickle"
-        recognizer = pickle.loads(open(recognizerPath, "rb").read())
 
-        lePath = "../../data/train_output/le.pickle"
-        le = pickle.loads(open(lePath, "rb").read())
+        self.recognizerPath = rospy.get_param('~recognizerPath')
+        #recognizerPath = "../../data/train_output/recognizer.pickle"
+        recognizer = pickle.loads(open(self.recognizerPath, "rb").read())
+
+        self.lePath = rospy.get_param('~lePath')
+        #lePath = "../../data/train_output/le.pickle"
+        le = pickle.loads(open(self.lePath, "rb").read())
 
 
         while rospy is not shutdown():
-            if not self.new_image_ready:
-                        continue
+            if not self.detected_face_ready:
+                continue
 
-                # construct a blob for the face ROI, then pass the blob
-		        # through our face embedding model to obtain the 128-d
-		        # quantification of the face
+            # construct a blob for the face ROI, then pass the blob
+		    # through our face embedding model to obtain the 128-d
+		    # quantification of the face
 
             
-                # la variable face coge el valor del topic recibido por el nodo anterior
-                face = self.detected_face
+            # la variable face coge el valor del topic recibido por el nodo anterior
+            face = self.detected_face
 
-                #
-		        faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96),
-			     (0, 0, 0), swapRB=True, crop=False)
-		        embedder.setInput(faceBlob)
-		        vec = embedder.forward()
-		        # perform classification to recognize the face
-		        preds = recognizer.predict_proba(vec)[0]
-		        j = np.argmax(preds)
-		        proba = preds[j]
-		        name = le.classes_[j]
+            #
+            faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96),(0, 0, 0), swapRB=True, crop=False)
+            embedder.setInput(faceBlob)
+            vec = embedder.forward()
+		    # perform classification to recognize the face
+            preds = recognizer.predict_proba(vec)[0]
+            j = np.argmax(preds)
+            proba = preds[j]
+            name = le.classes_[j]
 
-                # draw the bounding box of the face along with the associated
-		        # probability
-		        text = "{}: {:.2f}%".format(name, proba * 100)
-		        y = startY - 10 if startY - 10 > 10 else startY + 10
-		        cv2.rectangle(image, (startX, startY), (endX, endY),
-			      (0, 0, 255), 2)
-		        cv2.putText(image, text, (startX, y),
-			        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+            # draw the bounding box of the face along with the associated
+		    # probability
+            text = "{}: {:.2f}%".format(name, proba * 100)
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(image, (startX, startY), (endX, endY),(0, 0, 255), 2)
+            cv2.putText(image, text, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
-                # show the output image (publicada como topic mediante ROS)
-                face_frame = self.bridge.cv2_to_imgmsg(face)
-                self.pub_cara.publish(face_frame)
+            # show the output image (publicada como topic mediante ROS)
+            face_frame = self.bridge.cv2_to_imgmsg(face)
+            self.pub_cara.publish(face_frame)
              
 
-                # Pronunciar el nombre en voz alta
+            # Pronunciar el nombre en voz alta
 
 
-                 #
+            #
     #leer el topic de face_detection_node
     def callback(self,data):
-            if self.new_image_ready:
+            if self.detected_face_ready:
                 return
             try:
                 self.detected_face = self.bridge.imgmsg_to_cv2(data, 'bgr8')
