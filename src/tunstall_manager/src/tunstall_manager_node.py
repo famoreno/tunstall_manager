7341ba4b7d7682177c8629f6c3d7a0b0a0206603
RobertitoGC <titorobertalto@gmail.com>
2022-10-18 13:10:59 +0200

    servicio delete

--- src/tunstall_manager/src/tunstall_manager_node.py
+++ src/tunstall_manager/src/tunstall_manager_node.py
@@ -7,7 +7,7 @@ import json
 from enum import Enum, auto
 from std_msgs.msg import String
 
-from tunstall_manager.srv import command_tunstall_manager,command_save_file,command_delete_all_history,command_delete_single_history
+from tunstall_manager.srv import command_tunstall_manager, command_delete_file
 
 # TODO: 
 # 
@@ -22,7 +22,7 @@ class TSensor:
 	sensor_position = (0.0,0.0)
 	sensor_room = "Unknown"
 	sensor_type = "Unknown"
-	sensor_status = False
+	sensor_status = (False,0)
 
 class tunstall_manager_node:
 	def __init__(self) -> None:
@@ -31,22 +31,13 @@ class tunstall_manager_node:
 		self.rate = rospy.Rate(1)
 		self.sensor_database = None
 		self.sensor_file = rospy.get_param('~sensorFile')
+		self.verbose = rospy.get_param('~verbose')
 
 		#Service to start/stop surveillance mode
 		
 		self.srv = rospy.Service('~command_tunstall_manager', command_tunstall_manager, self.handle_command_tunstall_manager)
 		
-		#Service to register sensor changes
-
-		self.srv = rospy.service('~command_save_file', command_save_file, self.handle_command_save_file)
-
-		#Service to restart databse
-
-		self.srv = rospy.service('~command_delete_all_history', command_delete_all_history, self.handle_command_delete_all_history)
-
-		#Service to delete a single element of the history
-
-		self.srv = rospy.service('~command_delete_single_history', command_delete_single_history, self. handle_command_delete_single_history)
+		self.srv = rospy.Service('~command_delete_file',command_delete_file, self.handle_command_delete_file)
 
 		self.load_sensors_from_file()
 
@@ -65,6 +56,9 @@ class tunstall_manager_node:
 			self.rate.sleep()
 	
 	def tunstall_trigger_callback(self,data):
+		# if self.active:
+			#
+
         # find the sensor in the database,
         # change its status and
         # according to its type, perfom an action (basically GO and RECOGNIZE and TALK)
@@ -95,15 +89,65 @@ class tunstall_manager_node:
 		else:
 			sensor['type'] = TSensorType.UNKNOWN
 
-	def handle_command_tunstall_manager(self,req):
+	def get_sensor_type(self, sensor):
 
+		# auxiliary method to convert sensor variable to a string type
+		if sensor['type'] == TSensorType.CHAIR : 
+			sensor['type'] = 'CHAIR'
+		elif sensor['type'] == TSensorType.DOOR:
+			sensor['type'] = 'DOOR'
+		elif sensor['type'] == TSensorType.PIR:
+			sensor['type'] = 'PIR'
+		elif sensor['type'] == TSensorType.UNKNOWN:
+			sensor['type'] = 'UNKOWN'
+
+	def save_db(self):
+		json_file = open(self.sensor_file,"w")
+		json.dump(self.sensor_db_dict, json_file)
+		json_file.close()
+
+
+	
+	def handle_command_tunstall_manager(self,req,sensor):
 		#Check whether we start the node or we stop it
 		if req.task_command == "on":
 			self.active = True
 			if self.verbose:
 				rospy.loginfo("[tunstall_manager_node] Received command: ON")
-				
-				return True
+
+			
+
+
+		#Check if we save the state of the sensor
+		elif req.task_command == "save":
+			
+			if self.verbose:
+				rospy.loginfo("[tunstall_manager_node] Received command: SAVE")
+
+			# Saving the data on a Json file
+			self.save_db()
+			
+			return True
+			
+			
+		
+		#check if we delete all history
+		elif req.task_command == "restart":
+			
+			if self.verbose:
+				rospy.loginfo("[tunstall_manager_node] Recieved command: RESTART")
+
+			# Restarting database
+			#json_file = open(self.sensor_file,"w")
+			#self.sensor_db_dict = json_file
+
+			for sensor in self.sensor_db_dict['sensor_db']:
+			#sensorValue is a custom variable that contains if the sensor is  on/off at any given moment
+				sensor ["status"] = []
+
+			#json.dump(self.sensor_db_dict, json_file)
+			#json_file.close()
+
 
 		elif req.task_command == "off":
 			self.active = False
@@ -111,105 +155,32 @@ class tunstall_manager_node:
 				rospy.loginfo("[tunstall_manager_node] Received command: OFF")
 				
 			return True
-		else:
-			return False
-
-	def handle_command_save_file(self,sensor,req):
-		#Check if we save the state of the sensor
-		if req.task_command == "save":
-			self.active = True
-			if self.verbose:
-				rospy.loginfo("[tunstall_manager_node] Recieved command: SAVE")
-
-				return True
-			
-			# auxiliary method to convert sensor variable to a string type
-			if sensor['type'] == TSensorType.CHAIR : 
-				sensor['type'] = 'CHAIR'
-			elif sensor['type'] == TSensorType.DOOR:
-				sensor['type'] = 'DOOR'
-			elif sensor['type'] == TSensorType.PIR:
-				sensor['type'] = 'PIR'
-			elif sensor['type'] == TSensorType.UNKNOWN:
-				sensor['type'] = 'UNKOWN'
-
-			# Saving the data on a Json file
-			json_file = open(self.sensor_file,"w")
-			self.sensor_db_dict = json_file
-
-			for sensor in self.sensor_db_dict['sensor_db']:
-				#sensorValue is a custom variable that contains if the sensor is  on/off at any given moment
-				sensor ["status"] = sensorValue
-
-			json.dump(self.sensor_db_dict, json_file)
-			json_file.close()
 
 		else: 
 			return False
 
-	def handle_command_delete_all_history(req,self,sensor):
-		#check if we delete all history
-		if req.task_command == "restart":
-			self.active = True
-			if self.verbose:
-				rospy.loginfo("[tunstall_manager_node] Recieved command: RESTART")
+	def handle_command_delete_file(self,req,sensor):
 
-				return True
-
-		# auxiliary method to convert sensor variable to a string type
-			if sensor['type'] == TSensorType.CHAIR : 
-				sensor['type'] = 'CHAIR'
-			elif sensor['type'] == TSensorType.DOOR:
-				sensor['type'] = 'DOOR'
-			elif sensor['type'] == TSensorType.PIR:
-				sensor['type'] = 'PIR'
-			elif sensor['type'] == TSensorType.UNKNOWN:
-				sensor['type'] = 'UNKOWN'
-
-		# Restarting database
-			json_file = open(self.sensor_file,"w")
-			self.sensor_db_dict = json_file
-
-			for sensor in self.sensor_db_dict['sensor_db']:
-				#sensorValue is a custom variable that contains if the sensor is  on/off at any given moment
-				sensor ["status"] = False
-
-			json.dump(self.sensor_db_dict, json_file)
-			json_file.close()
-
-	def handle_command_delete_single_history(req,sensor,self):
-		#check if we want delete a single element
+		
+		
+			#check if we want delete a single element
 		if req.task_command == "delete":
-			self.active = True
 			if self.verbose:
-				rospy.loginfo("[tunstall_manager_node] Recieved command: DELETE")
-
-				return True
+				rospy.loginfo("[tunstall_manager_node] Received command: DELETE")
 			
-			# auxiliary method to convert sensor variable to a string type
-			if sensor['type'] == TSensorType.CHAIR : 
-				sensor['type'] = 'CHAIR'
-			elif sensor['type'] == TSensorType.DOOR:
-				sensor['type'] = 'DOOR'
-			elif sensor['type'] == TSensorType.PIR:
-				sensor['type'] = 'PIR'
-			elif sensor['type'] == TSensorType.UNKNOWN:
-				sensor['type'] = 'UNKOWN'
-
+			for sensor in self.sensor_db_dict['sensor_db']:
+				
+				if req.id == sensor.id: break
+				else: rospy.loginfo("[ERROR: id not found")
+			
+		
 			# deleting file
+
 			json_file = open(self.sensor_file,"w")
-			self.sensor_db_dict = json_file
+			self.sensor_db_dict[0]['status'] = []
+			#self.sensor_db_dict = json_file
 
-			
-
-			
-
-
-
-
-			
-			
-
+		
 
 def main(args):
   rospy.init_node('tunstall_manager_node', anonymous=True)
